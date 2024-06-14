d53e46bbda290b9b4f9c4005e1062f8e216ccd5a
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-06-14 17:08:58 -0400

    a

--- flake.nix
+++ flake.nix
@@ -1,35 +1,27 @@
-# vim:ts=2:sts=2:sw=2:et:ft=nix:
+# vim: ft=nix ts=2 sts=2 sw=2 et
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  outputs =
-    { self, nixpkgs }:
-    let
-      withPkgs = f: nixpkgs.lib.mapAttrs (k: v: f v) nixpkgs.legacyPackages;
-    in
-    {
-      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
-      packages = withPkgs (pkgs: {
-        default = derivation {
-          name = "oxium";
-          inherit (pkgs) system;
-          builder = "${pkgs.coreutils}/bin/install";
-          args = [
-            "-D"
-            (derivation {
-              name = "oxium-bin";
-              inherit (pkgs) system;
-              builder = "${pkgs.rustc}/bin/rustc";
-              args = [
-                "${./.}/shell.rs"
-                "-C"
-                "linker=${pkgs.gcc}/bin/gcc"
-                "-o"
-                (builtins.placeholder "out")
-              ];
-            })
-            "${builtins.placeholder "out"}/bin/oxium"
-          ];
-        };
-      });
+  inputs.stddev.url = "github:PoolloverNathan/stddev";
+  outputs = {
+    self,
+    stddev,
+  }:
+    stddev rec {
+      name = "oxium";
+      # deps = pkgs: [pkgs.rustc ]
+      packages = system: pkgs: {
+        default = let
+          mf = (pkgs.lib.importTOML ./Cargo.toml).package;
+        in
+          assert mf.name == name;
+          pkgs.rustPlatform.buildRustPackage rec {
+            pname = mf.name;
+            version = mf.version;
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
+            nativeBuildInputs = [pkgs.pkg-config pkgs.nix pkgs.openssl pkgs.openssl.dev];
+            PKG_CONFIG_PATH = ["${pkgs.openssl.dev}/lib/pkgconfig/"];
+          };
+      };
+      deps = pkgs: let p = packages pkgs.system pkgs; in p.default.buildInputs ++ p.default.nativeBuildInputs;
     };
 }
